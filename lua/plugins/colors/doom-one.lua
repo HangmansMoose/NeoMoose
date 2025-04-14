feb338df924ab8eb2f2c68b4acb4816c7397c2a5
HangmansMoose <36186205+HangmansMoose@users.noreply.github.com>
2025-04-14 17:11:32 +1000

    Delete lua directory

--- lua/plugins/colors/doom-one.lua
+++
@@ -1,6 +0,0 @@
-return {
-    {
-        'NTBBloodbath/doom-one.nvim',
-        name = 'doom-one',
-    },
-}
--- lua/plugins/treesitter.lua
+++
@@ -1,7 +0,0 @@
--- Treesitter conf moved to config/treesitter_conf.lua to make it easier to configure the compilers
-
-return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-  }
-
